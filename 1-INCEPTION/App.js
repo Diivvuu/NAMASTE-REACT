a86269060eb7e6570d4ba05c272baa37cf3e3cbf
Diivvuu <divyanshubawa9@gmail.com>
2024-04-16 23:44:24 +0530

    new

--- 1-INCEPTION/App.js
+++ 1-INCEPTION/App.js
@@ -1,16 +1,21 @@
-const parent = React.createElement("div", { id: "parent" }, [
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm and h1 tag"),
-    React.createElement("h2", {}, "I'm and h2 tag"),
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "I'm an h1 tag"),
     React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+  ])
+);
 
-console.log(parent); //object
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello from React"
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(parent);
